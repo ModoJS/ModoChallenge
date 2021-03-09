21af4ea7614d3602a50e7cccf10eea48f2713680
hernanSoftland <harena@softland.com.ar>
2021-03-09 01:03:04 -0300

    actions creado

---
+++ dist/tests/controllers/movement-test.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const chai_1 = __importDefault(require("chai"));
+var assert = chai_1.default.assert;
+var should = chai_1.default.should();
+var expect = chai_1.default.expect;
+describe('Testing del controlador de movimientos: ', function () {
+    describe('Revisión de la busqueda de movmientos', function () {
+        it('Debe traer todos los movimientos de los ultimos 7 dias', function () {
+        });
+    });
+});
+//# sourceMappingURL=movement-test.js.map
---
+++ test/controllers/movement.test.js
@@ -0,0 +1,3 @@
+test("Verificar que la paginacion funciona con exito", () => {
+    expect("Hola mundo").toBe("Hola mundo");
+});
